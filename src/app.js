89854c302d42b7614af345bbef1c9f42b900758b
jjdabid <jaimedavidp@hotmail.com>
2024-08-26 17:08:14 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT||8080;
+
+app.listen(PORT, ()=>console.log(`listening on port ${PORT}`));
+
+app.get('/api/products', (req, res) => { res.send("Todos los productos") });
+
+app.get('/api/products/:pid', (req, res) => { res.send("Producto por ID") });
